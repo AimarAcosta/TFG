fe4f0aca333c161089de754488676934537ba5c8
Aimar Acosta <aimar.acostaal@elorrieta-errekamari.com>
2026-04-20 10:47:50 +0200

    Conectado con firebase

--- src/app/features/partidos/dashboard/dashboard.ts
+++ src/app/features/partidos/dashboard/dashboard.ts
@@ -5,7 +5,6 @@ import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
-  standalone: true,
   imports: [CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
